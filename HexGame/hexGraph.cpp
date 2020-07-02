b83ae4958b7e35a60817a21d663448c9de85236e
Mustafa Al-tekreeti <mmaj1976@gmail.com>
2020-07-01 21:15:21 -0400

    Sixth commit

--- HexGame/hexGraph.cpp
+++ HexGame/hexGraph.cpp
@@ -1,8 +1,8 @@
 /*
 Implementation of the hex game:
+
 */
 #include"hexGraph.h"
-#include"c:\Users\Mustafa\source\repos\Dijkstra7\Dijkstra7\ShortestPathAlgo.h"
 
 #include<iostream>
 #include<ctime>
@@ -62,60 +62,59 @@ void hexGraph::print(vector<hexState>& hexplace) {
 	}
 }
 
-bool hexGraph::game_over(char stone, vector<hexState>& hexplace) {
+bool hexGraph::is_connected(unsigned s, unsigned e, char stone, vector<hexState>& hexplace) {
+	unordered_set<unsigned> done_set, doing_set;
+	unsigned node;
 	hexState int_stone;
-	vector<vector<unsigned>> graph(nNode*nNode);
-	vector<unsigned> nodes(nNode*nNode);
-	unsigned k = 0;//number of nodes of the Dijksrta graph
 	if (stone == 'X')
 		int_stone = XPLR;
 	else
 		int_stone = OPLR;
-	//In this part, we extract the sub-graph that corresponds to either human or machine moves
-	for (size_t i = 0; i < nNode; i++)
-		for (size_t j = 0; j < nNode; j++) {
-			if (hexplace[i*nNode + j] == int_stone) {
-				graph[k] = edgelist[i*nNode + j];
-				nodes[k] = i * nNode + j;
-				k = k + 1;
-			}
-		}
-	graph.resize(k);
-	nodes.resize(k);
-	graph.shrink_to_fit();
-	nodes.shrink_to_fit();
-	vector<unsigned> i_start, i_end;//indices of start and end nodes
-	for (size_t i = 0; i < k; i++){
+	doing_set.insert(s);
+	while (doing_set.empty() != true) {
+		node = *doing_set.begin();
+		doing_set.erase(node);
+		for (auto v : edgelist[node])
+			if (hexplace[v] == int_stone && done_set.find(v) == done_set.end())
+				if (v == e)
+					return true;
+				else
+					doing_set.insert(v);
+		done_set.insert(node);
+	}
+	return false;
+}
+
+bool hexGraph::game_over(char stone, vector<hexState>& hexplace) {
+	hexState int_stone;
+	if (stone == 'X')
+		int_stone = XPLR;
+	else
+		int_stone = OPLR;
+	vector<unsigned> n_start, n_end;//start and end nodes
+	n_start.reserve(nNode);
+	n_end.reserve(nNode);
+	for (size_t i = 0; i < nNode; i++){
 		//double check whether any of the start and end nodes are among the moves 
 				if (int_stone == XPLR) {//horizontally
-					if (nodes[i] % nNode == 0)
-						i_start.push_back(nodes[i]);//ith node is a horizantal start node
-					if ((nodes[i] + 1) % nNode == 0) 						
-						i_end.push_back(nodes[i]);//ith node is a horizantal end node				
+					if (hexplace[i*nNode] == int_stone)
+						n_start.push_back(i*nNode);//ith node is a horizantal start node
+					if (hexplace[(nNode-1)+i*nNode] == int_stone)
+						n_end.push_back((nNode - 1) + i * nNode);//ith node is a horizantal end node				
 				}
 				else {//vertically
-					if (nodes[i] < nNode) 						
-						i_start.push_back(nodes[i]);//ith node is a vertical start node					
-					if (nodes[i] > nNode*(nNode - 1)) 					
-						i_end.push_back(nodes[i]);//ith node is a vertical end node					
+					if (hexplace[i] == int_stone)
+						n_start.push_back(i);//ith node is a vertical start node					
+					if (hexplace[nNode*(nNode - 1)+i] == int_stone) 					
+						n_end.push_back(nNode*(nNode - 1) + i);//ith node is a vertical end node					
 				}
 	}
-	if (i_start.size() == 0 || i_end.size() == 0)
+	if (n_start.size() == 0 || n_end.size() == 0)
 		return false;//the game is for sure not over
-	//evaluate the graph using Dijkstra
-	Graph g(k, nodes, graph);
-	ShortestPathAlgo spa(g);
-	for (auto x:i_start) {
-		auto it_s = find(nodes.cbegin(), nodes.cend(), x);
-		auto index_s = distance(nodes.cbegin(), it_s);//index of the start node
-		for (auto y:i_end){
-			auto it_e = find(nodes.cbegin(), nodes.cend(), y);
-			auto index_e = distance(nodes.cbegin(), it_e);//index of the end node
-			list<int> nlist = spa.path(index_s,index_e);
-			if (nlist.size() != 0)//is there any path from north (west) to south (east)
-				return true;//game over, one of the players win the game
-		}
-	}				
+	for (auto x:n_start)
+		for (auto y:n_end)
+			if (is_connected(x, y, stone, hexplace))
+				return true;
 	return false;//nobody win the game
 }
 
@@ -163,8 +162,8 @@ void hexGraph::hex_game() {
 		}
 		else {//machine round
 			while (true) {
-				move = rand() % (nNode*nNode - 1);//randomly select a move
-				//move = plausibile_move(machine, nNode*nNode - human_count - machine_count, 1000, hexplace);
+				//move = rand() % (nNode*nNode - 1);//randomly select a move
+				move = plausibile_move(machine, nNode*nNode - human_count - machine_count, 1000, hexplace);
 				if (hexplace[move] == EMPTY) {
 					if (machine == 'X')
 						hexplace[move] = XPLR;
